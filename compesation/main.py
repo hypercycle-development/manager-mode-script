3ceb96d76a6358237398f5e2ce055794b8abee07
NanezX <victor.nanezj@gmail.com>
2025-09-01 21:43:44 -0400

    rename variables

--- compesation/main.py
+++ compesation/main.py
@@ -6,11 +6,12 @@ from typing import Literal
 from gist_addresses import fetch_gist_addresses
 
 USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-addresses_gist_id = "1d91a306014cab0eefea297feab97e54"
+tranche1_addresses_gist_id = "1d91a306014cab0eefea297feab97e54"
 
 
 async def main():
-    addresses = await fetch_gist_addresses(addresses_gist_id)
+    # Get all the HTS Tranche1 address
+    t1_addresses = await fetch_gist_addresses(tranche1_addresses_gist_id)
 
 
 if __name__ == "__main__":
