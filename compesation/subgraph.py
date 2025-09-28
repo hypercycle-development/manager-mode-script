d1d5f038c468b8cce8c5a835034c686a24534193
NanezX <victor.nanezj@gmail.com>
2025-09-28 14:06:24 -0400

    add new condition at subgraph query

--- compesation/subgraph.py
+++ compesation/subgraph.py
@@ -45,9 +45,9 @@ def build_query_for_licenses(ADDRESS: str, BLOCK_NUMBER: int = MAX_BLOCK_NUMBER)
             orderBy: licenseId
             where: {{
                 or: [
-                    {{ rTokenHolders_: {{ holder: "{ADDRESS}", amount_gt: 0 }}, operatorString_not: "TO_BE_REPLACED" }}
-                    {{ wTokenHolders_: {{ holder: "{ADDRESS}", amount_gt: 0 }}, operatorString_not: "TO_BE_REPLACED" }}
-                    {{ operator: "{ADDRESS}", operatorString_not: "TO_BE_REPLACED" }}
+                    {{ rTokenHolders_: {{ holder: "{ADDRESS}", amount_gt: 0 }}, operatorString_not: "TO_BE_REPLACED", status_not: ENDED }}
+                    {{ wTokenHolders_: {{ holder: "{ADDRESS}", amount_gt: 0 }}, operatorString_not: "TO_BE_REPLACED", status_not: ENDED }}
+                    {{ operator: "{ADDRESS}", operatorString_not: "TO_BE_REPLACED", status_not: ENDED }}
                 ]
             }}
             block: {{ number: {BLOCK_NUMBER} }}
